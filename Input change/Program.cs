d053ea76040523dbce3ecdf240296a49339ca337
My NoteBook <My NoteBook@DESKTOP-3K165PF>
2023-05-17 11:46:28 -0700

    Add project files.

---
+++ Input change/Program.cs
@@ -0,0 +1,35 @@
+﻿// this program change A and B with a value C
+
+
+//int a = 5;
+//int b = 9;
+//int c = a;
+
+//a = b;
+//c = b;
+
+//System.Console.WriteLine("The value of A is: ", a);
+//System.Console.WriteLine("The value of B is: " , b);
+
+////////////// with user input
+
+System.Console.Write("Enter A: ");
+string astring = System.Console.ReadLine(); 
+int firstNumber = System.Convert.ToInt32(astring);
+
+System.Console.Write("Enter B: ");
+string bstring = System.Console.ReadLine(); 
+int secondNumber = System.Convert.ToInt32(bstring);
+
+int c = 0;
+
+c = firstNumber;
+firstNumber = secondNumber;
+secondNumber = c;
+
+System.Console.WriteLine("Your swap value is " + firstNumber + " " + secondNumber);
+
+
+
+
+
